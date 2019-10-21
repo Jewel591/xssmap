b4ffca2140949169072d13e29fd879bbe1f7c9f5
Jewel591 <Zhc456654456>
2019-10-21 12:42:15 +0800

    增加 referer 参数的检测

--- modules/precheck.py
+++ modules/precheck.py
@@ -41,40 +41,55 @@ class PreCheck():
         targetvar = urldata.targetvar #去除参数末尾的空格——输入1
         if targetvar=="":
             targetvar = "height"
+        revar0 = targetvar + "="
         revar1 = targetvar + "=.*?&"
         revar2 = targetvar + "=.*"
-        if not re.search("(POST)", urldata.targeturl):
+        if len(re.findall(revar0, urldata.targeturl)) > 1:
+            print("\033[1;31;8m[警告] "+"匹配到 > 1 个"+urldata.targetvar+"，默认使用第一个，请确认目标参数是否替换正确"+"\033[0m")
+        if len(re.findall(revar0, urldata.targeturl)) < 1:
+            print("\033[1;31;8m[警告] " + "url 中未匹配到" + urldata.targetvar + "，请确认目标参数是否输入正确" + "\033[0m")
+            sys.exit(0)
+        if not re.search("(POST)", urldata.targeturl) and not re.search("(REFERER)", urldata.targeturl) and not re.search("(COOKIE)", urldata.targeturl):
             urldata.HTTP_METHON = "GET"
             if re.search(revar1, urldata.targeturl):
                 urldata.get_url = re.sub(
                     revar1, targetvar + "=" + "abcdef1234&", urldata.targeturl)
                 print(">>> 清除敏感字符: ", urldata.get_url)
-                # format.breakline()
             else:
                 urldata.get_url = re.sub(
                     revar2, targetvar + "=" + "abcdef1234", urldata.targeturl)
                 print(">>> 清除敏感字符: ", urldata.get_url)
-                # format.breakline()
         else:
-            urldata.HTTP_METHON = "POST"
-            url_split_list = re.split(re.escape("(POST)"), urldata.targeturl)
-            urldata.post_url = url_split_list[0]
-            print(">>> posturl ", urldata.post_url)
-            # format.breakline()
-            print(">>> postdata ", url_split_list[1])
+            if re.search("(REFERER)", urldata.targeturl):
+                urldata.HTTP_METHON = "REFERER"
+                url_split_list = re.split(re.escape("(REFERER)"), urldata.targeturl)
+                urldata.referer_url = url_split_list[0]
+                print(">>> refererurl ", urldata.referer_url)
+                print(">>> referer 自动清空")
 
-            if re.search(revar1, url_split_list[1]):
-                urldata.post_data = re.sub(
-                    revar1,
-                    targetvar + "=" + "abcdef1234&",
-                    url_split_list[1])
-                print(">>> 清除敏感字符: ", urldata.post_data)
             else:
-                urldata.post_data = re.sub(
-                    revar2,
-                    targetvar + "=" + "abcdef1234",
-                    url_split_list[1])
-                print(">>> 清除敏感字符: ", urldata.post_data)
+                if re.search("(COOKIE)", urldata.targeturl):
+                    print("COOKIE 函数还没写")
+                    sys.exit(0)
+                else:
+                    urldata.HTTP_METHON = "POST"
+                    url_split_list = re.split(re.escape("(POST)"), urldata.targeturl)
+                    urldata.post_url = url_split_list[0]
+                    print(">>> posturl ", urldata.post_url)
+                    print(">>> postdata ", url_split_list[1])
+
+                    if re.search(revar1, url_split_list[1]):
+                        urldata.post_data = re.sub(
+                            revar1,
+                            targetvar + "=" + "abcdef1234&",
+                            url_split_list[1])
+                        print(">>> 清除敏感字符: ", urldata.post_data)
+                    else:
+                        urldata.post_data = re.sub(
+                            revar2,
+                            targetvar + "=" + "abcdef1234",
+                            url_split_list[1])
+                        print(">>> 清除敏感字符: ", urldata.post_data)
 
     def get_response(self, url, verbose):
         if verbose=="yes":
@@ -120,7 +135,7 @@ class PreCheck():
             s2=r.post(
                 urldata.post_url,
                 data=data,
-                proxies=proxy,
+                # proxies = proxy,
                 headers=header,
                 verify=False,
                 timeout=5).text
@@ -130,6 +145,63 @@ class PreCheck():
             if verbose == "yes":
                 print(">>> 连接错误")
             return "post no Response"
+
+    def referer_response(self, data, verbose):
+        if verbose == "yes":
+            print("[+] Referer : ",data)
+        else:
+            print(".", end='')
+            sys.stdout.flush()
+        # r = NoQuoteSession()
+        header = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Referer': data}
+        proxy = {"http": "http://127.0.0.1:8080",
+                 "https": "http://127.0.0.1:8080"}
+        try:
+            response2referer=requests.get(
+                urldata.referer_url,
+                # proxies = proxy,
+                headers=header,
+                verify=False,
+                timeout=5).text
+            return response2referer
+        # except requests.exceptions.ConnectionError:
+        except BaseException:
+            if verbose == "yes":
+                print(">>> 连接错误")
+            return "post no Response"
+
+
+    def cookie_response(self, data, verbose):
+        if verbose == "yes":
+            print("[+] Cookie : ",data)
+        else:
+            print(".", end='')
+            sys.stdout.flush()
+        r = NoQuoteSession()
+        header = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0',
+            'Content-Type': 'application/x-www-form-urlencoded'}
+        proxy = {"http": "http://127.0.0.1:8080",
+                 "https": "http://127.0.0.1:8080"}
+        try:
+            s2=r.post(
+                urldata.post_url,
+                data=data,
+                # proxies = proxy,
+                headers=header,
+                verify=False,
+                timeout=5).text
+            return s2
+        # except requests.exceptions.ConnectionError:
+        except BaseException:
+            if verbose == "yes":
+                print(">>> 连接错误")
+            return "post no Response"
+
+
 
     def get_response_burp(self, url):
         r = NoQuoteSession()
