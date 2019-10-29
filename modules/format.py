595d1396d81dabdd96fe341eb43d06de36f493d6
Jewel591 <Zhc456654456>
2019-10-29 15:53:56 +0800

    添加功能：增加输入目标链接后自动检测参数，方便指定目标参数 修复bug：对标签检测中 <> 进行 urlencode

--- modules/format.py
+++ modules/format.py
@@ -1,2 +1,9 @@
+from data import payload
 def breakline():
     print("------------------------------------------------------------")
+
+def payloadReplace(llist,lvar,rvar):
+    rlist = []
+    for i in llist:
+        rlist.append(i.replace(lvar, rvar))
+    return rlist
