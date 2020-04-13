7ed9dce3205843ca8787f26720a27a5eb7a20d35
Jewel <1106901808@qq.com>
2020-04-13 09:48:42 +0800

    Update install.sh

--- docs/install.sh
+++ docs/install.sh
@@ -1,1 +1,11 @@
-git clone https://github.com/Jewel591/CheckXSS.git && cd CheckXSS && pip3 install -r requirement.txt -i https://pypi.douban.com/simple && echo "Successful installation"
+echo "repository name: CheckXSS by Jewel591"
+git clone https://github.com/Jewel591/CheckXSS.git 
+if [ "$?"!= "0" ]; then
+   echo "[error] git clone failed ..."
+   exit 1
+cd CheckXSS
+pip3 install -r requirement.txt -i https://pypi.douban.com/simple 
+if [ "$?"!= "0" ]; then
+   echo "[error] pip3 install failed "
+   exit 1
+echo "Successful installation, thanks"
