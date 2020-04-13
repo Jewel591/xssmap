bde0bd00e6523c08fb243d1c4894bfeb05f0dfdb
Jewel <1106901808@qq.com>
2020-04-13 10:01:46 +0800

    Update install.sh

--- docs/install.sh
+++ docs/install.sh
@@ -1,9 +1,11 @@
-echo "repository name: CheckXSS by Jewel591"
+echo -e ""
+echo -e "\033[34m==>  \033[0m\033[1mCloning Repository ... \033[0m"
 git clone https://github.com/Jewel591/CheckXSS.git 
 if [ "$?"!= "0" ]; then
    echo "[error] git clone failed ..."
    exit 1
 cd CheckXSS
+echo -e "\033[34m==>  \033[0m\033[1mInstall Python3 module ... \033[0m"
 pip3 install -r requirement.txt -i https://pypi.douban.com/simple 
 if [ "$?"!= "0" ]; then
    echo "[error] pip3 install failed "
