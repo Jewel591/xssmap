c9d848ed8223bb09849b9ac3ee93aabf0abc7940
Jewel591 <1106901808@qq.com>
2020-08-16 17:44:00 +0800

    Todo.md example.sh

--- docs/example.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# coding=utf-8
-
-"""
-codecov|https://codecov.io/gh/Jewel591/CheckXSS
-"""
-
-python3 ../xssmap.py -u "http://demo.testfire.net/search.jsp?query=windws" --ua="checkxss" --cookie="JSESSIONID=9B7ADB9D3DB7FC018F9FE641612558BE" -p query
